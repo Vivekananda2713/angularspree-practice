84d84dcd2439f4749aba4bda46283aae6c6f4d29
Vivekananda2713 <raikantivivekananda@gmail.com>
2025-05-01 20:57:01 +0000

    Code cleanup and optimization

---
+++ src/app/modules/search/components/search-filters-container/index.ts
@@ -0,0 +1,10 @@
+import { SearchFiltersContainerComponent } from './search-filters-container.component';
+import * as SFCComponents from './components';
+
+export const COMPONENTS = [
+  ...SFCComponents.COMPONENTS,
+  SearchFiltersContainerComponent
+];
+
+
+// Note: Could be optimized in future iterations
